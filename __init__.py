ce9810a3d97d252b0a90585334bad950e6a2942c
Simone Basso <bassosimone@gmail.com>
2015-05-20 21:43:05 +0200

    Update usage example

--- __init__.py
+++ __init__.py
@@ -14,16 +14,18 @@
      import json
      from neubot_scheduler import http
 
-     def simple(connection, request):
-         ''' Handles /URL '''
-         response = {
-             "method": request.method,
-             "url": request.url,
-             "protocol": request.protocol,
-             "headers": request.headers,
-             "body": request.body_as_string()
-         }
-         connection.write(http.writer.compose_response("200", "Ok", {
+     class Simple(http.HTTPRequestHandler):
+         ''' Handles /simple URL '''
+
+         def on_end(self, connection, request):
+             response = {
+                 "method": request.method,
+                 "url": request.url,
+                 "protocol": request.protocol,
+                 "headers": request.headers,
+                 "body": request.body_as_string()
+             }
+             connection.write(http.writer.compose_response("200", "Ok", {
                  "Content-Type": "application/json",
              }, json.dumps(response, indent=4)))
 
@@ -31,14 +33,16 @@
          ''' Main function '''
          http.listen({
              "routes": {
-                 "/simple": simple,
+                 "/simple": Simple,
              }
          })
          asyncore.loop()
+
+    main()
 """
 
 __version__ = 2.0
 
 from .file_handler import FileHandler
-from .core import listen
+from .core import HTTPRequestHandler, listen
 from . import writer
